a0e1118dd0fe5d863a44752c04b50916319ddedf
Prathamesh Bhiku Jadhav <37609950+prathmesh6464@users.noreply.github.com>
2020-02-11 18:13:57 +0530

    Delete Leap.py

--- Leap.py
+++
@@ -1,9 +0,0 @@
-year=(input("Enter year: "))
-if len(year)==4:
-	year=int(year)
-	if(year%4==0 and year%100!=0) or year%400==0:
-		print(year,"is Leap Year")
-	else:
-				print(year,"is Not Leap Year")
-else:
-	print("Enter 4 digit Year Please")
