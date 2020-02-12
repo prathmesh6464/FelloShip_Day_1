67dd3176ac0eb824763cce1aaaa94a69b66ea82e
pratham <prathmesh6464@gmail.com>
2020-02-12 12:15:39 +0530

    dskfdjsfkljsdfkljsdklfjsdfkl

--- AnagramString.py
+++ AnagramString.py
@@ -1,2 +1,17 @@
-string1=input("Enter First String")
-string2=input("Enter First String")
+string1=input("Enter First String").split()
+string2=input("Enter First String").split()
+def sorting(strings):
+	for i in range(len(strings)):
+		minindex=i
+		j=i
+		while j<len(strings):
+			if strings[minindex]>strings[j]:
+				minindex=j
+			j+=1
+		strings[i],strings[minindex]=strings[minindex],strings[i]
+	print(strings)
+	return strings
+if sorting(string1)==sorting(string2):
+	print("String is anagram ")
+else:
+	print("String is Not Anagram")
